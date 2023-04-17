07ca98b4462ebc2766373268983158be86fee1ca
Igor <igor.mogilnov@mail.ru>
2023-04-17 10:15:43 +0300

    добавлен метод для заполнения второго массива значениями соответсвующими условию задания

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -6,3 +6,16 @@ Console.Clear();
 
 string[] array1 = new string[4] {"Hello", "2", "world", ":-)"};
 string[] array2 = new string[array1.Length];
+
+void FillArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
