09f15ee3ce45643cac7c1eb981a1c29537246a45
Igor <igor.mogilnov@mail.ru>
2023-04-17 10:17:49 +0300

    добавлены метод для печати массива и строки вызова методов

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -19,3 +19,13 @@ void FillArray(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+for (int i = 0; i < array.Length; i++)
+{
+Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+}
+FillArray(array1, array2);
+PrintArray(array2);
